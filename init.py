54eb45638f07b403d1dc266dcbe978b57f1c44f0
ReDAndFel <pipecar366@gmail.com>
2024-04-18 14:58:52 -0500

    Se agrego un comentario para dejar especificados los cambos que se deben llenar en el app.config

--- init.py
+++ init.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import and_
 
 app = Flask(__name__)
+#mysql+mysqlconnector://usuario_database:password_database@host_database/name_database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:fel1004684293@localhost/api_logs'
 db = SQLAlchemy(app)
 
